1a03b0bc218205a1645a02c41418cc7e9a6dda65
Michael Hofmann <mhofmann@redhat.com>
2021-12-07 19:42:50 +0100

    Day 6
    Signed-off-by: Michael Hofmann <mhofmann@redhat.com>

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ mod day_02;
 mod day_03;
 mod day_04;
 mod day_05;
+mod day_06;
 
 fn run(puzzle: &str) -> Option<String> {
     match puzzle {
@@ -17,6 +18,8 @@ fn run(puzzle: &str) -> Option<String> {
         "4/2" => Some(day_04::part_2("data/puzzle-04-input")),
         "5/1" => Some(day_05::part_1("data/puzzle-05-input")),
         "5/2" => Some(day_05::part_2("data/puzzle-05-input")),
+        "6/1" => Some(day_06::part_1("data/puzzle-06-input")),
+        "6/2" => Some(day_06::part_2("data/puzzle-06-input")),
         _ => {
             println!("Unknown puzzle");
             None
