84c3b42c4b3974195fe301ca418fd9cca511c34a
Michael Hofmann <mhofmann@redhat.com>
2021-12-08 18:24:57 +0100

    Day 8

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,7 @@ mod day_2021_04;
 mod day_2021_05;
 mod day_2021_06;
 mod day_2021_07;
+mod day_2021_08;
 
 fn run(puzzle: &str) -> Option<String> {
     match puzzle {
@@ -29,6 +30,8 @@ fn run(puzzle: &str) -> Option<String> {
         "2021/6/2" => Some(day_2021_06::part_2("data/puzzle-2021-06-input")),
         "2021/7/1" => Some(day_2021_07::part_1("data/puzzle-2021-07-input")),
         "2021/7/2" => Some(day_2021_07::part_2("data/puzzle-2021-07-input")),
+        "2021/8/1" => Some(day_2021_08::part_1("data/puzzle-2021-08-input")),
+        "2021/8/2" => Some(day_2021_08::part_2("data/puzzle-2021-08-input")),
         _ => {
             println!("Unknown puzzle");
             None
